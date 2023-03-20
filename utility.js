dd98222c406bbcb2c410fb775fa6040933704da4
palamedes <jason.ellis@yahoo.com>
2023-03-20 11:57:32 -0400

    Update

--- utility.js
+++ utility.js
@@ -1,16 +1,12 @@
 module.exports = {
 
+  // Get the max energy capacity of the room (not how much we have, but how much we COULD have)
   getMaxEnergyCapacity: room => {
-//      let baseCapacity = STRUCTURE_SPAWN.energyCapacity + STRUCTURE_EXTENSION.energyCapacity * CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][room.controller.level];
     let extensions = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION } });
     let extensionCapacity = _.sum(extensions, (extension) => extension.energyCapacity);
     let spawnCapacity = Game.spawns[Object.keys(Game.spawns)[0]].energyCapacity;
     console.log(extensionCapacity + spawnCapacity);
     return extensionCapacity + spawnCapacity;
   },
-
-  getSkavenBodyParts: size => {
-    return [WORK, CARRY, MOVE, MOVE, MOVE];
-  }
 
 };
