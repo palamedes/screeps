9ac735b9d52d3bdaba57b0075c4b2edc74c16cdf
palamedes <jason.ellis@yahoo.com>
2023-03-21 20:37:39 -0400

    Okay this works.. I hope

--- role.skaven.js
+++ role.skaven.js
@@ -46,24 +46,22 @@ var roleSkaven = {
     let ratName = 'Skaven-' + Game.time;
     let ratBrain = { memory: { role: 'skaven', task: null, slept: 0, attempted: 0, ...memory } };
     // Calculate the number of body parts based on energySize
-    let numWork  = Math.floor(energy * 0.50 / 100);
+    let numWork  = Math.floor(energy * 0.50 / 100); // 50% of the energy to work
     energy = energy - numWork * 100;
-    let numCarry = Math.floor(energy * 0.50 / 50);
+    let numCarry = Math.floor(energy * 0.50 / 50); // 50% of the remaining energy to carry
     energy = energy - numCarry * 50;
-    let numMove  = Math.floor(energy / 50);
+    let numMove  = Math.floor(energy / 50); // 100% remaining to move
     energy = energy - numMove * 50;
+    let numTough = Math.floor(energy / 10); // Any amount left over, add toughness
 
     // Build the array of body parts based on the calculated numbers
     let ratParts = [];
     for (let i = 0; i < numWork; i++)   { ratParts.push(WORK); }
     for (let i = 0; i < numCarry; i++)  { ratParts.push(CARRY); }
     for (let i = 0; i < numMove; i++)   { ratParts.push(MOVE); }
-    // Any amount left over, add toughness
-    let numTough = Math.floor(energy / 10);
-    for (let i = 0; i < numTough; i++) { ratParts.push(TOUGH); }
+    for (let i = 0; i < numTough; i++)  { ratParts.push(TOUGH); }
 
-    console.log(ratParts);
-    // Game.spawns["Toiletduck's Nest"].spawnCreep(ratParts, ratName, ratBrain);
+    Game.spawns["Toiletduck's Nest"].spawnCreep(ratParts, ratName, ratBrain);
   },
 }
 module.exports = roleSkaven;
