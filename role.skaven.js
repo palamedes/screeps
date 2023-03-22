862ba1294310834920cc6ecf35ef86c03ae9e4c6
palamedes <jason.ellis@yahoo.com>
2023-03-22 16:51:17 -0400

    Adding "slave" (role of skaven will go away since that's going to be the identifier for the whole lot) Less verbose (just use emojis)

--- role.skaven.js
+++ role.skaven.js
@@ -4,45 +4,46 @@ const utility = require('utility');
 var roleSkaven = {
 
   skitter: rat => {
-    let maxSkaven = Memory.maxSkaven;
-    let skaven = _.filter(Game.creeps, (rat) => rat.memory.role === 'skaven');
-    let constructionTargets = rat.room.find(FIND_CONSTRUCTION_SITES);
-    let repairTargets = skavenActions.repair.getRepairTargets(rat);
-    let upgradeTarget = rat.room.controller;
-    if (rat.memory.role === 'skaven') {
+    if (rat.memory.role === 'skaven' || rat.memory.role === 'slave') {
+      let maxSkaven = Memory.maxSkaven;
+      let slave = _.filter(Game.creeps, (rat) => rat.memory.role === 'skaven' || rat.memory.role === 'slave');
+      let constructionTargets = rat.room.find(FIND_CONSTRUCTION_SITES);
+      let repairTargets = skavenActions.repair.getRepairTargets(rat);
+      let upgradeTarget = rat.room.controller;
       // Determine what we should be doing...
       if (!rat.memory.task) {
         // If they have no free capacity for energy, then go do some work.
         if (rat.store.getFreeCapacity() === 0) {
           // Construction comes first... If we have 50% or more rats, and we don't have more than 50% doing the work
-          if (constructionTargets.length > 0 && skaven.length >= (maxSkaven/2) && skavenActions.numActive('build') <= (maxSkaven*0.5)) {
+          if (constructionTargets.length > 0 && slave.length >= (maxSkaven/2) && skavenActions.numActive('build') <= (maxSkaven*0.5)) {
             rat.memory.task = 'build';
             rat.memory.slept = 0;
-            rat.say('🚧Build');
+            rat.say('🚧');
           }
           // Repair comes second... If we have 50% or more rats, and we have 20% or less repairing
-          else if (repairTargets.length > 0 && skaven.length >= (maxSkaven/2) && skavenActions.numActive('repair') <= (maxSkaven*0.2)) {
+          else if (repairTargets.length > 0 && slave.length >= (maxSkaven/2) && skavenActions.numActive('repair') <= (maxSkaven*0.2)) {
             rat.memory.task = 'repair';
             rat.memory.slept = 0;
-            rat.say('🔧Repair');
+            rat.say('🛠️');
           }
-          // Upgrade comes third... But only if we have 80% of max skaven and then only 20% can do the work..
+          // Upgrade comes third... But only if we have 80% of max slaves and then only 20% can do the work..
           // or if we have slept a while.. Meaning there is nothing else to do.. go upgrade.
-          else if (upgradeTarget && ((skaven.length >= (maxSkaven*0.8) && skavenActions.numActive('upgrade') <= (maxSkaven*0.2)) || rat.memory.slept > 8)) {
+          else if (upgradeTarget && ((slave.length >= (maxSkaven*0.8) && skavenActions.numActive('upgrade') <= (maxSkaven*0.2)) || rat.memory.slept > 5)) {
             rat.memory.task = 'upgrade';
             rat.memory.slept = 0;
-            rat.say('🔧Upgrade');
+            rat.say('🔧');
           }
           else {
             rat.memory.task = 'store';
             rat.memory.slept = 0;
-            rat.say('⚡Store');
+            rat.say('🔋');
           }
+          //🪫
         } else {
           rat.memory.task = 'harvest';
           rat.memory.myTargetId = null;
           rat.memory.slept = 0;
-          rat.say('⛏️Harvest');
+          rat.say('⚡');
         }
       }
       // Okay rat... Do something..
@@ -52,7 +53,7 @@ var roleSkaven = {
 
   // Spawn us a rat ~ Standard Skaven worker rat
   summonSkaven: (energy, memory) => {
-    if (energy >= 300) { skavenActions.summonSkaven(energy, memory); return ' ~ Spawning new Skaven ('+energy+')' } else { return ''; }
+    if (energy >= 300) { skavenActions.summonSkavenSlave(energy, memory); return ' ~ Spawning new Skaven ('+energy+')' } else { return ''; }
   },
 
   // Spawn us a rat ~ Standard Skaven worker rat
