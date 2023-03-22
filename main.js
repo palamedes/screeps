54866e850f326b93c83586341e745aa4224af8d6
palamedes <jason.ellis@yahoo.com>
2023-03-21 22:32:38 -0400

    Moving stuff around

--- main.js
+++ main.js
@@ -11,13 +11,11 @@ module.exports.loop = function () {
   let energyAvailable = room.energyAvailable;
   let energyAvailableComment = 'Room "'+Game.spawns[Object.keys(Game.spawns)[0]].room.name+'" has ' + room.energyAvailable + ' energy';
   let skaven = _.filter(Game.creeps, (rat) => rat.memory.role === 'skaven');
-  let ogres = _.filter(Game.creeps, (rat) => rat.memory.role === 'ogre');
+  let ogres  = _.filter(Game.creeps, (rat) => rat.memory.role === 'ogre');
 
   // Log Output
   let statusUpdate = energyAvailableComment + ' ~ Slaves: ' + skaven.length;
-  if (ogres.length > 0) {
-    statusUpdate += ', Ogres: ' + orgres.length;
-  }
+  if (ogres.length > 0) { statusUpdate += ', Ogres: ' + orgres.length; }
 
   // Delete memory of old dead creeps
   for(var name in Memory.creeps) { if(!Game.creeps[name]) { delete Memory.creeps[name]; }}
@@ -27,15 +25,13 @@ module.exports.loop = function () {
   let extensionCapacity = _.sum(extensions, (extension) => extension.energyCapacity);
   let spawnCapacity = Game.spawns[Object.keys(Game.spawns)[0]].energyCapacity;
   let maxEnergyCapacity = extensionCapacity + spawnCapacity;
-  // Spawn a skaven
+  // Spawn a skaven slave
   if ((skaven.length < 2 || (skaven.length < Memory.maxSkaven && energyAvailable >= maxEnergyCapacity)) && energyAvailable >= 200) {
-    statusUpdate += ' ~ Spawning new Skaven ('+energyAvailable+')';
-    roleSkaven.summonSkaven(energyAvailable, { roomBound: Game.spawns[Object.keys(Game.spawns)[0]].room.name });
+    statusUpdate += roleSkaven.summonSkaven(energyAvailable, { roomBound: Game.spawns[Object.keys(Game.spawns)[0]].room.name });
   }
   // Spawn a rat ogre
   if (ogres < Memory.maxOgres && skaven.length === Memory.maxSkaven && energyAvailable >= maxEnergyCapacity) {
-    statusUpdate += ' ~ Spawning new Rat Ogre ('+energyAvailable+')';
-    roleSkaven.summonRatOgre(energyAvailable, { roomBound: Game.spawns[Object.keys(Game.spawns)[0]].room.name });
+    statusUpdate += roleSkaven.summonRatOgre(energyAvailable, { roomBound: Game.spawns[Object.keys(Game.spawns)[0]].room.name });
   }
   // Work the rats
   for(let name in Game.creeps) { var rat = Game.creeps[name]; roleSkaven.skitter(rat); }
