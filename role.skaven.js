2ccbfd5ab4b5ffc0f0e4fae7e3694d3bc48e1503
palamedes <jason.ellis@yahoo.com>
2023-03-21 22:36:39 -0400

    Only do skaven

--- role.skaven.js
+++ role.skaven.js
@@ -9,37 +9,39 @@ var roleSkaven = {
     let constructionTargets = rat.room.find(FIND_CONSTRUCTION_SITES);
     let repairTargets = skavenActions.repair.getRepairTargets(rat);
     let upgradeTarget = rat.room.controller;
-    // Determine what we should be doing...
-    if (!rat.memory.task) {
-      // If they have no free capacity for energy, then go do some work.
-      if (rat.store.getFreeCapacity() === 0) {
-        // Construction comes first... If we have 5 or more rats, and we dont have more than 4 doing the work
-        if (constructionTargets.length > 0 && skaven.length >= (maxSkaven/2) && skavenActions.numActive('build') <= (maxSkaven*0.4)) {
-          rat.memory.task = 'build';
-          rat.say('🚧Build');
+    if (rat.memory.role === 'skaven') {
+      // Determine what we should be doing...
+      if (!rat.memory.task) {
+        // If they have no free capacity for energy, then go do some work.
+        if (rat.store.getFreeCapacity() === 0) {
+          // Construction comes first... If we have 5 or more rats, and we dont have more than 4 doing the work
+          if (constructionTargets.length > 0 && skaven.length >= (maxSkaven/2) && skavenActions.numActive('build') <= (maxSkaven*0.4)) {
+            rat.memory.task = 'build';
+            rat.say('🚧Build');
+          }
+          // Repair comes second... If we have 5 or more rats, and we have 2 or less doing the work.
+          else if (repairTargets.length > 0 && skaven.length >= (maxSkaven/2) && skavenActions.numActive('repair') <= (maxSkaven*0.2)) {
+            rat.memory.task = 'repair';
+            rat.say('🔧Repair');
+          }
+          // Upgrade comes third... But only if we have 80% of max skaven and then only 40% can do the work
+          else if (upgradeTarget && skaven.length >= (maxSkaven*0.8) && skavenActions.numActive('upgrade') <= (maxSkaven*0.4)) {
+            rat.memory.task = 'upgrade';
+            rat.say('🔧Upgrade');
+          }
+          else {
+            rat.memory.task = 'store';
+            rat.say('⚡Store');
+          }
+        } else {
+          rat.memory.task = 'harvest';
+          rat.memory.myTargetId = null;
+          rat.say('⛏️Harvest');
         }
-        // Repair comes second... If we have 5 or more rats, and we have 2 or less doing the work.
-        else if (repairTargets.length > 0 && skaven.length >= (maxSkaven/2) && skavenActions.numActive('repair') <= (maxSkaven*0.2)) {
-          rat.memory.task = 'repair';
-          rat.say('🔧Repair');
-        }
-        // Upgrade comes third... But only if we have 80% of max skaven and then only 40% can do the work
-        else if (upgradeTarget && skaven.length >= (maxSkaven*0.8) && skavenActions.numActive('upgrade') <= (maxSkaven*0.4)) {
-          rat.memory.task = 'upgrade';
-          rat.say('🔧Upgrade');
-        }
-        else {
-          rat.memory.task = 'store';
-          rat.say('⚡Store');
-        }
-      } else {
-        rat.memory.task = 'harvest';
-        rat.memory.myTargetId = null;
-        rat.say('⛏️Harvest');
       }
+      // Okay rat... Do something..
+      skavenActions.skitter(rat);
     }
-    // Okay rat... Do something..
-    skavenActions.skitter(rat);
   },
 
   // Spawn us a rat ~ Standard Skaven worker rat
