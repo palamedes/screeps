862ecb5a63c08d2f0c0c9c037fb4ece6e04ed1d1
palamedes <jason.ellis@yahoo.com>
2023-03-22 11:57:32 -0400

    highlight dropp

--- skaven.harvest.js
+++ skaven.harvest.js
@@ -18,7 +18,20 @@ let sHarvest = {
     });
 
     if (!rat.memory.myTargetId && droppedEnergy.length > 0) {
+      let firstDrop = droppedEnergy[0];
       console.log('assigning dropped energy');
+      let x = firstDrop.pos.x;
+      let y = firstDrop.pos.y;
+      let roomName = firstDrop.pos.roomName;
+
+      new RoomVisual(roomName).circle(x, y, {
+        radius: 0.5,
+        fill: 'rgba(255, 255, 0, 0.3)',
+        stroke: 'yellow',
+        strokeWidth: 0.15
+      });
+
+      // THIS FIRES but it then just goes on to harvest, is a regular energy source considered a dropped energy source?
       rat.memory.myTargetId = droppedEnergy.id
     }
 
