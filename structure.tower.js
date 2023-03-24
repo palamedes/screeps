db088eeaaf39ad73ba52930eef43cdae638d02e1
palamedes <jason.ellis@yahoo.com>
2023-03-23 20:57:32 -0400

    Adding an attack and fixing the heal

--- structure.tower.js
+++ structure.tower.js
@@ -1,14 +1,32 @@
 var structureTower = {
 
+  // Heal any damaged rats near by
   heal: () => {
-    for (let structureId in Game.structures) {
-      let structure = Game.structures[structureId];
-      if (structure.structureType === STRUCTURE_TOWER) {
-        var closestDamagedCreep = structure.pos.findClosestByRange(FIND_MY_CREEPS, {
-          filter: (creep) => creep.hits < creep.hitsMax
+    for (let towerId in Game.towers) {
+      let tower = Game.towers[towerId];
+      if (tower.towerType === STRUCTURE_TOWER) {
+        var damagedRat = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
+          filter: (rat) => rat.hits < rat.hitsMax
         });
-        if(closestDamagedCreep) {
-          structure.heal(closestDamagedCreep);
+        if(damagedRat) {
+          tower.heal(damagedRat);
+        }
+      }
+    }
+  },
+
+  // Attack any hostile creeps
+  attack: () => {
+    // Are there any hostile creeps?
+    const hostileCreeps = Game.spawns[Object.keys(Game.spawns)[0]].room.find(FIND_HOSTILE_CREEPS);
+    if (hostileCreeps.length > 0) {
+      for (let towerId in Game.towers) {
+        let tower = Game.towers[towerId];
+        if (tower.towerType === STRUCTURE_TOWER) {
+          let closestHostile = tower.pos.findClosestByRange(FIND_HOSTILE_CREEPS);
+          if (closestHostile) {
+            tower.attack(closestHostile);
+          }
         }
       }
     }
