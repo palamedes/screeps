451cb98ac74b313c06fe936135d88a6ac739442b
palamedes <jason.ellis@yahoo.com>
2023-03-24 09:43:42 -0400

    we still moving?

--- skaven.move.js
+++ skaven.move.js
@@ -2,6 +2,7 @@ let sMove = {
 
   // have our rat move to a location
   moveTo: (rat, target, stroke) => {
+    console.log(rat.name)
     let options = { noPathFinding: true, visualizePathStyle: { stroke: stroke } }
     let path = sMove.memorizePath(rat, target);
     let res = rat.moveByPath(path, options)
