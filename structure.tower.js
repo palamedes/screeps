d5170dc535281387a0ac132b1db5e9bd97d8b4d1
palamedes <jason.ellis@yahoo.com>
2023-03-23 21:15:11 -0400

    Fix!

--- structure.tower.js
+++ structure.tower.js
@@ -13,41 +13,33 @@ var structureTower = {
   // Heal any damaged rats near by
   heal: towers => {
     for (let tower in towers) {
-      // let tower = Game.towers[towerId];
-      if (tower.towerType === STRUCTURE_TOWER) {
-        var damagedRat = tower.pos.findClosestByRange(FIND_MY_CREEPS, {
-          filter: (rat) => rat.hits < rat.hitsMax
-        });
-        if(damagedRat) {
-          tower.heal(damagedRat);
-        }
+      let damagedRat = towers[id].pos.findClosestByRange(FIND_MY_CREEPS, {
+        filter: (rat) => rat.hits < rat.hitsMax
+      });
+      if (damagedRat) {
+        towers[id].heal(damagedRat);
       }
     }
   },
 
   // Repair those things that need repairing
   repair: towers => {
-    for (let tower in towers) {
-      // let tower = Game.towers[towerId];
-      if (tower.towerType === STRUCTURE_TOWER) {
-        let damagedStructures = tower.room.find(FIND_STRUCTURES, {
-          filter: (structure) => structure.hits < structure.hitsMax
-        });
-        if (damagedStructures.length > 0) {
-          damagedStructures.sort((a, b) => a.hits - b.hits);
-          tower.repair(damagedStructures[0]);
-        }
+    for (let id in towers) {
+      let damagedStructures = towers[id].room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.hits < structure.hitsMax
+      });
+      if (damagedStructures.length > 0) {
+        damagedStructures.sort((a, b) => a.hits - b.hits);
+        towers[id].repair(damagedStructures[0]);
       }
     }
   },
 
   // Attack any hostile creeps
   attack: towers => {
-    console.log('testin attack')
     // Are there any hostile creeps?
     const hostileCreeps = Game.spawns[Object.keys(Game.spawns)[0]].room.find(FIND_HOSTILE_CREEPS);
     if (hostileCreeps.length > 0) {
-      console.log('enemy!')
       for (let id in towers) {
         // let tower = Game.towers[towerId];
         let closestHostile = towers[id].pos.findClosestByRange(FIND_HOSTILE_CREEPS);
