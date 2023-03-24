0221a56d6484c894353f21544bf55f66872b5c39
palamedes <jason.ellis@yahoo.com>
2023-03-23 21:27:14 -0400

    If there are towers, dont' have a rat repair

--- skaven.repair.js
+++ skaven.repair.js
@@ -1,3 +1,5 @@
+const structureTower = require('structure.tower');
+
 let sRepair = {
   // Go find something to repair
   using: (rat) => {
@@ -22,19 +24,25 @@ let sRepair = {
     }
   },
 
+  // Get any repair targets for this rat
   getRepairTargets: rat => {
-    return rat.room.find(FIND_STRUCTURES, {
-      filter: (structure) => {
-        if (structure.structureType == STRUCTURE_ROAD) {
-          return structure.hits < structure.hitsMax * 0.8; // repair roads at 80% of maximum hits
-        } else if (structure.structureType === STRUCTURE_WALL || structure.structureType === STRUCTURE_RAMPART) {
-          return structure.hits < structure.hitsMax * 0.0001;
-        } else {
-          return (structure.structureType != STRUCTURE_CONTROLLER) &&
-            structure.hits < structure.hitsMax;
+    let towers = structureTower.getTowers();
+    if (towers) {
+      return null;
+    } else {
+      return rat.room.find(FIND_STRUCTURES, {
+        filter: (structure) => {
+          if (structure.structureType === STRUCTURE_ROAD) {
+            return structure.hits < structure.hitsMax * 0.8; // repair roads at 80% of maximum hits
+          } else if (structure.structureType === STRUCTURE_WALL || structure.structureType === STRUCTURE_RAMPART) {
+            return structure.hits < structure.hitsMax * 0.0001;
+          } else {
+            return (structure.structureType !== STRUCTURE_CONTROLLER) &&
+              structure.hits < structure.hitsMax;
+          }
         }
-      }
-    });
+      });
+    }
   },
 
 }
