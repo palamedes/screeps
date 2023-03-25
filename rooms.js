1d59853f15ddad0240e4eca8747dee6dbb885926
palamedes <jason.ellis@yahoo.com>
2023-03-25 18:22:16 -0400

    initial commit of rooms

---
+++ rooms.js
@@ -0,0 +1,15 @@
+const roleSkaven = require("./role.skaven");
+
+let rooms = {
+  run: room => {
+    if (room.memory.status === 'init') { rooms.init(room); }
+  },
+  // Setup plan for base, roads to sources..etc.
+  init: room => {
+    console.log('init the room');
+  },
+  running: () => {
+
+  }
+}
+module.exports = rooms;
