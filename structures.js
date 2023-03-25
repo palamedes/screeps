34993d92aea2f9d41759a8c13ae7bdda3ab7f297
palamedes <jason.ellis@yahoo.com>
2023-03-25 19:59:09 -0400

    wow that worked?!

--- structures.js
+++ structures.js
@@ -27,7 +27,7 @@ let structures = {
     // Plan the room and store it in memory
     Memory.rooms[room.name].basePlan = Memory.rooms[room.name].basePlan || structures.basePlan(room);
     // Function to take the spiral string, and actually draw it out on the map.
-    const drawSpiral = (start, str, rv) => {
+    const drawSpiral = (start, str, rV) => {
       const x = start.x, y = start.y;
       let dx = 0, dy = -1, len = 0, posX = x, posY = y, index = 0;
       while (index < str.length) {
