5ffcda43a0ff23dd65e0755602d9ff9b4cc5b9c6
palamedes <jason.ellis@yahoo.com>
2023-03-25 18:24:22 -0400

    Memory

--- rooms.js
+++ rooms.js
@@ -2,8 +2,7 @@ const roleSkaven = require("./role.skaven");
 
 let rooms = {
   run: room => {
-    console.log(room.memory.status);
-    if (room.memory.status === 'init') { rooms.init(room); }
+    if (Memory.rooms[room.name].status  === 'init') { rooms.init(room); }
   },
   // Setup plan for base, roads to sources..etc.
   init: room => {
