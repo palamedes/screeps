477c6abb06da80301fd7537ef6aa97a3f38746e4
palamedes <jason.ellis@yahoo.com>
2023-03-25 19:10:59 -0400

    refactoring how this works

--- structures.js
+++ structures.js
@@ -24,22 +24,51 @@ let structures = {
 
 
   drawBaseplan: room => {
-    structures.basePlan(room);
-  } ,
-
-  // okay this may seem heavy handed but this is a good way for me to get my brain around what I think the base should
-  // sorta look like.  this is a stamp, but only in that it's a rough guess as to what it should look like and it will
-  // dynamically alter itself during the spiral draw to fit the terrain.
-  basePlan: room => {
     const spawn = room.find(FIND_MY_SPAWNS)[0];
     const roomName = room.name;
     const rV = new RoomVisual(roomName);
+    // Plan the room and store it in memory
+    structures.basePlan(room);
+    // Function to take the spiral string, and draw it back to the map.
+    // Save the sprial map (string of characters) to the rooms memory.. this is our base plan!
+    const drawSpiral = (start, str, rv) => {
+      const x = start.x, y = start.y;
+      let dx = 0, dy = -1, len = 0, posX = x, posY = y, index = 0;
+      while (index < str.length) {
+        for (let i = 0; i < len; i++) {
+          if (index < str.length) {
+            let c = str.charAt(index);
+            if (c !== " ") {
+              let terrain = Game.map.getRoomTerrain(roomName);
+              if (terrain.get(posX, posY) !== TERRAIN_MASK_WALL) {
+                if (hasWallInAdjacentSquares(posX, posY, terrain)) { c = '#'; }
+                rV.text(c, posX, posY, {opacity: 0.8, font: 0.5, color: 'red'});
+              }
+            }
+            index++;
+          }
+          posX += dx;
+          posY += dy;
+        }
+        [dx, dy] = [-dy, dx];
+        if (dy === 0) {
+          len++;
+        }
+      }
+    }
+    // Draw the beast on the map
+    drawSpiral(spawn.pos, Memory.rooms[room.name].spiralStamp, rV);
+  } ,
 
+  // Okay this may seem heavy handed but this is a good way for me to get my brain around what I think the base should
+  // sorta look like.  this is a stamp, but only in that it's a rough guess as to what it should look like and it will
+  // dynamically alter itself during the spiral draw to fit the terrain and then the bot itself will add what it wants
+  // over time.  # are roads, e are extensions, T are towers, · can be anything..etc..
+  basePlan: room => {
+    const roomName = room.name;
     // RESOURCE_*, MINERAL_*, CREEP, TOWER, SOURCE, CONTROLLER, POWER_BANK, POWER_SPAWN,
     // RUIN, PORTAL, LAB, SPAWN, LINK, WALL, EXTENSION, RAMPART, ROAD.
     // @ = SPAWN,  # = ROAD,  T = TOWER,  e = EXTENSION, · = {dynamic anything}
-    // const plan = "@,#Tee#eeT,eT.e#e#.#.#e#ee#"
-
     const basePlan = [];
     basePlan[0] = "###############";
     basePlan[1] = "##·····#·····##";
@@ -56,7 +85,6 @@ let structures = {
     basePlan[12]= "#·#····#····#·#";
     basePlan[13]= "##·····#·····##";
     basePlan[14]= "###############";
-
     // Check cardinal directions for walls
     let hasWallInAdjacentSquares = (x,y,terrain) => {
       return (terrain.get(x-1, y) === TERRAIN_MASK_WALL) ||
@@ -65,7 +93,7 @@ let structures = {
              (terrain.get(x, y+1) === TERRAIN_MASK_WALL)
     }
     // Convert the above stamp, to a spiral starting at the main base "*" (6,8)
-    // *eeT#Tee#·#e#ee#eT·e#e#·... etc.. around and around expanding outwards
+    // *eeT#Tee#·#e#ee#eT·e#e#·... etc.. around and around expanding outwards.  This allows us to dynamically change as we draw.
     // This was hard to figure out.. And kinda pointless, its only ever run once typically.. but still was a fun challenge.
     const spiralStamp = (basePlan, startX, startY) => {
       const dirs = [[-1, 0], [0, -1], [1, 0], [0, 1]];
@@ -103,39 +131,7 @@ let structures = {
       }
       return plan;
     };
-    const drawSpiral = (start, str, rv) => {
-      const x = start.x, y = start.y;
-      let dx = 0, dy = -1, len = 0, posX = x, posY = y, index = 0;
-      while (index < str.length) {
-        for (let i = 0; i < len; i++) {
-          if (index < str.length) {
-            let c = str.charAt(index);
-            if (c !== " ") {
-              let terrain = Game.map.getRoomTerrain(roomName);
-              if (terrain.get(posX, posY) !== TERRAIN_MASK_WALL) {
-                if (hasWallInAdjacentSquares(posX, posY, terrain)) {
-                  c = '#';
-                }
-                rV.text(c, posX, posY, {opacity: 0.8, font: 0.5, color: 'red'});
-                // Create Memory base plan array that houses what should be on the spot we are drawing on.
-
-              }
-            }
-            index++;
-          }
-          posX += dx;
-          posY += dy;
-        }
-        [dx, dy] = [-dy, dx];
-        if (dy === 0) {
-          len++;
-        }
-      }
-    }
-
     Memory.rooms[room.name].spiralStamp = Memory.rooms[room.name].spiralStamp || spiralStamp(basePlan,7, 7);
-
-    drawSpiral(spawn.pos, Memory.rooms[room.name].spiralStamp, rV);
   }
 
 }
