8f035fa27bda5de587dad834e157c0bd0959f973
palamedes <jason.ellis@yahoo.com>
2023-03-25 22:16:37 -0400

    oh *2

--- main.js
+++ main.js
@@ -37,15 +37,9 @@ module.exports.loop = function () {
     let controllerLevel = room.controller.level
 
 
-    let numSucklePoints = () => {
-      let count = 0;
-      for (let i in Memory.rooms[room.name].sources) {
-        count = count + Memory.rooms[room.name].sources[i].length;
-      }
-      return count;
-    }
-    // Rejigger max slaves for this room based on the level of the room
-    Memory.rooms[room.name].maxSlaves = numSucklePoints();
+    let numSucklePoints = () => { return Object.values(Memory.rooms[room.name].sources).reduce((acc, val) => acc + val.length, 0); }
+    // Rejigger max slaves for this room based on the number of suckle points..
+    Memory.rooms[room.name].maxSlaves = numSucklePoints() * 2;
 
 
     // Spawn a skaven slave
