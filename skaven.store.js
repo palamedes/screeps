02017297ca1a00dc32d5124285175cfaeb77d36a
palamedes <jason.ellis@yahoo.com>
2023-03-27 19:10:30 -0400

    change order of storing

--- skaven.store.js
+++ skaven.store.js
@@ -2,29 +2,24 @@ const move = require("./skaven.move");
 let sStore = {
   // Go store the energy
   using: rat => {
-    var targets = rat.room.find(FIND_STRUCTURES, {
-      filter: (structure) => {
-        return (structure.structureType === STRUCTURE_EXTENSION ||
-                structure.structureType === STRUCTURE_SPAWN ||
-                structure.structureType === STRUCTURE_CONTAINER ||
-                structure.structureType === STRUCTURE_TOWER) &&
-                structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0;
-      },
-      sort: (a, b) => {
-        const types = [ STRUCTURE_CONTAINER, STRUCTURE_TOWER, STRUCTURE_SPAWN, STRUCTURE_EXTENSION ];
-        return types.indexOf(a.structureType) - types.indexOf(b.structureType);
-      }
-    });
+    let targets = rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_EXTENSION && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+    targets.push(rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_SPAWN && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+    targets.push(rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_TOWER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+    targets.push(rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_CONTAINER && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+
     console.log('store Targets:',targets);
     if (rat.store.getUsedCapacity() === 0) {
       rat.memory.myTargetId = null;
       rat.memory.task = null;
     }
+    // If there are any targets store in order above..
     if(targets.length > 0) {
-      var target = targets[0];
-      // var target = rat.pos.findClosestByRange(targets);
-      if(rat.transfer(target, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-        move.moveTo(rat, target, '#aaaaaa');
+      if(rat.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        move.moveTo(rat, targets[0], '#aaaaaa');
       }
       return true;
     }
