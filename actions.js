bed2fbbe5defd3d39d351c5099874d44b14f099d
palamedes <jason.ellis@yahoo.com>
2023-03-26 21:24:37 -0400

    Update harvester

--- actions.js
+++ actions.js
@@ -57,7 +57,7 @@ let $actions = {
     let percentWork = 0.5, percentCarry = 0.50;
     // If we have more than 2 slaves already, and we don't have as many dedicated harvesters as we need.. get one
     if (slaves.length >= 2 && numHarvesters < Memory.rooms[room.name].numSucklePoints) {
-      percentWork = 0.75; percentCarry = 0;
+      percentWork = 0.85; percentCarry = 0;
     }
 
     let energy = room.energyAvailable;
