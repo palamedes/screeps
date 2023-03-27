2e0323b10544fcbf7af45b71e16ddb54af3a4c58
palamedes <jason.ellis@yahoo.com>
2023-03-27 16:07:13 -0400

    lets think this through

--- main.js
+++ main.js
@@ -47,7 +47,7 @@ module.exports.loop = function () {
     if (Memory.rooms[room.name].numSucklePoints == 3) { maxSlaves = 8; }
     if (Memory.rooms[room.name].numSucklePoints == 4) { maxSlaves = 12; }
 
-    if (room.controller.level == 4) { maxSlaves--; }
+    // if (room.controller.level == 4) { maxSlaves--; }
 
 
 
