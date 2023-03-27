5563ee5a8e6b81eb2170c599447fc5d26c52dc67
palamedes <jason.ellis@yahoo.com>
2023-03-27 19:18:56 -0400

    test

--- skaven.store.js
+++ skaven.store.js
@@ -4,14 +4,22 @@ let sStore = {
   using: rat => {
     let targets = rat.room.find(FIND_STRUCTURES, {
       filter: (structure) => structure.structureType === STRUCTURE_EXTENSION  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
-    targets.push(rat.room.find(FIND_STRUCTURES, {
-      filter: (structure) => structure.structureType === STRUCTURE_SPAWN      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
-    targets.push(rat.room.find(FIND_STRUCTURES, {
-      filter: (structure) => structure.structureType === STRUCTURE_TOWER      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
-    targets.push(rat.room.find(FIND_STRUCTURES, {
-      filter: (structure) => structure.structureType === STRUCTURE_CONTAINER  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
-    targets.push(rat.room.find(FIND_STRUCTURES, {
-      filter: (structure) => structure.structureType === STRUCTURE_STORAGE    && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+    if (targets.length === 0) {
+      targets = rat.room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.structureType === STRUCTURE_SPAWN      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+    }
+    if (targets.length === 0) {
+      targets = rat.room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.structureType === STRUCTURE_TOWER      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+    }
+    if (targets.length === 0) {
+      targets = rat.room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.structureType === STRUCTURE_CONTAINER      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+    }
+    if (targets.length === 0) {
+      targets = rat.room.find(FIND_STRUCTURES, {
+        filter: (structure) => structure.structureType === STRUCTURE_STORAGE      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+    }
 
     if (rat.store.getUsedCapacity() === 0) {
       rat.memory.myTargetId = null;
