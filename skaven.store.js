34978e9b6cb378e1234f7be372b1798903d2d356
palamedes <jason.ellis@yahoo.com>
2023-03-27 19:16:46 -0400

    undoing that

--- skaven.store.js
+++ skaven.store.js
@@ -2,28 +2,27 @@ const move = require("./skaven.move");
 let sStore = {
   // Go store the energy
   using: rat => {
+    let targets = rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_EXTENSION  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+    targets.push(rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_SPAWN      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+    targets.push(rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_TOWER      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+    targets.push(rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_CONTAINER  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+    targets.push(rat.room.find(FIND_STRUCTURES, {
+      filter: (structure) => structure.structureType === STRUCTURE_STORAGE    && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
+
     if (rat.store.getUsedCapacity() === 0) {
       rat.memory.myTargetId = null;
       rat.memory.task = null;
-    } else {
-      let targets = rat.room.find(FIND_STRUCTURES, {
-        filter: (structure) => structure.structureType === STRUCTURE_EXTENSION  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
-      targets.push(rat.room.find(FIND_STRUCTURES, {
-        filter: (structure) => structure.structureType === STRUCTURE_SPAWN      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
-      targets.push(rat.room.find(FIND_STRUCTURES, {
-        filter: (structure) => structure.structureType === STRUCTURE_TOWER      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
-      targets.push(rat.room.find(FIND_STRUCTURES, {
-        filter: (structure) => structure.structureType === STRUCTURE_CONTAINER  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
-      targets.push(rat.room.find(FIND_STRUCTURES, {
-        filter: (structure) => structure.structureType === STRUCTURE_STORAGE    && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 }));
-
-      // If there are any targets store in order above..
-      if(targets.length > 0) {
-        if(rat.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
-          move.moveTo(rat, targets[0], '#aaaaaa');
-        }
-        return true;
+    }
+    // If there are any targets store in order above..
+    if(targets.length > 0) {
+      if(rat.transfer(targets[0], RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        move.moveTo(rat, targets[0], '#aaaaaa');
       }
+      return true;
     }
     return false;
   },
