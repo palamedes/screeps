84c05cb73dc1e5751bca2e2aa7472e1f77f6693f
palamedes <jason.ellis@yahoo.com>
2023-03-27 18:53:08 -0400

    testing

--- skaven.harvest.js
+++ skaven.harvest.js
@@ -53,7 +53,7 @@ let sHarvest = {
     // Now that you have found a target, Go to that target and harvest it, assuming it has power.
     let target = Game.getObjectById(rat.memory.myTargetId);
     // console.log('target:',target);
-    if (target && target.energy > 0 && target !== true) {
+    if (target && target.energy > 0) {
 
       console.log("I found that " + rat.memory.myTargetId + " yields a:", target);
 
