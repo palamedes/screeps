95c36d392001f7711248688857eb8cc935278fc0
palamedes <jason.ellis@yahoo.com>
2023-03-27 18:43:14 -0400

    testing

--- skaven.harvest.js
+++ skaven.harvest.js
@@ -55,7 +55,7 @@ let sHarvest = {
     console.log('target:',target);
     if (target && target.energy > 0 && target !== true) {
 
-      console.log("I found that " + rat.memory.myTargetId + " yields a:", target);
+      // console.log("I found that " + rat.memory.myTargetId + " yields a:", target);
 
       // If the target is a pickup, then go try to pick it up
       if (target instanceof Resource && rat.pickup(target) === ERR_NOT_IN_RANGE) {
