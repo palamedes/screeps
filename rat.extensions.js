14ef5af0c6a5601d16315183370425d509a0490d
palamedes <jason.ellis@yahoo.com>
2023-03-28 22:19:32 -0400

    Testing extending Creep

---
+++ rat.extensions.js
@@ -0,0 +1,7 @@
+// Utility to find all available spawns
+Creep.prototype.getAvailableSpawn = () => {
+  const spawns = this.room.find(FIND_MY_STRUCTURES, {
+    filter: (structure) => structure.structureType === STRUCTURE_SPAWN  && !structure.spawning
+  });
+  return spawns.length > 0 ? this.pos.findClosestByRange(spawns) : false;
+};
