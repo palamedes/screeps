74357d1c0e9b35cd6dd5a7afbcf296a10f255bdf
palamedes <jason.ellis@yahoo.com>
2023-03-28 22:41:05 -0400

    Adding more extensions

--- rat.extensions.js
+++ rat.extensions.js
@@ -1,4 +1,4 @@
-// Utility to find all available spawns
+
 Creep.prototype.getAvailableSpawn = function() {
   const spawns = this.room.find(FIND_MY_STRUCTURES, {
     filter: (structure) => structure.structureType === STRUCTURE_SPAWN  && !structure.spawning
@@ -6,7 +6,8 @@ Creep.prototype.getAvailableSpawn = function() {
   return spawns.length > 0 ? this.pos.findClosestByRange(spawns) : false;
 };
 
-// Can this Creep Carry?
-Creep.prototype.canCarry = function() {
-  return this.body.some(part => part.type === CARRY);
-}
+Creep.prototype.canCarry = function() { return this.body.some(part => part.type === CARRY); }
+Creep.prototype.cannotCarry = function() { return this.body.every(part => part.type !== CARRY); }
+
+Creep.prototype.canWork = function() { return this.body.some(part => part.type === WORK); }
+Creep.prototype.cannotWork = function() { return this.body.every(part => part.type !== WORK); }
