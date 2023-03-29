ff1ed96923c70ca0ca2023bd85653b6e57e1a5b9
palamedes <jason.ellis@yahoo.com>
2023-03-29 08:59:55 -0400

    make sure the rat can work to upgrade

--- skaven.upgrade.js
+++ skaven.upgrade.js
@@ -3,7 +3,7 @@ let sUpgrade = {
   // Go upgrade the room controller. (Note; if a rat is bored it will also do this task without the task being set)
   using: rat => {
     var target = rat.room.controller;
-    if (rat.room.controller && rat.store.getUsedCapacity(RESOURCE_ENERGY) > 0) {
+    if (rat.room.controller && rat.store.getUsedCapacity(RESOURCE_ENERGY) > 0 && rat.canWork()) {
       if (rat.upgradeController(rat.room.controller) === ERR_NOT_IN_RANGE) {
         move.moveTo(rat, rat.room.controller, '#00ff00');
       }
