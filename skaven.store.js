9b0e8896524d5c9d9f12fe12c077b43f1ee225ef
palamedes <jason.ellis@yahoo.com>
2023-03-29 16:20:27 -0400

    send target

--- skaven.store.js
+++ skaven.store.js
@@ -2,18 +2,21 @@ const move = require("./skaven.move");
 let sStore = {
   // Go store the energy
   using: rat => {
-    let targets = [];
+    let targets = [], target = null;
 
     // If the rat cannot WORK then it's probably a hauler so check for more storage
     if (rat.cannotWork()) {
       if (targets.length === 0) { targets = rat.room.find(FIND_STRUCTURES, {
         filter: (structure) => structure.structureType === STRUCTURE_TOWER      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+        target = targets.sort((a, b) => a.store.getFreeCapacity(RESOURCE_ENERGY) - b.store.getFreeCapacity(RESOURCE_ENERGY))[0];
       }
       if (targets.length === 0) { targets = rat.room.find(FIND_STRUCTURES, {
         filter: (structure) => structure.structureType === STRUCTURE_CONTAINER  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+        target = rat.pos.findClosestByRange(targets);
       }
       if (targets.length === 0) { targets = rat.room.find(FIND_STRUCTURES, {
         filter: (structure) => structure.structureType === STRUCTURE_STORAGE    && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+        const target = rat.pos.findClosestByRange(targets);
       }
     }
 
@@ -21,20 +24,22 @@ let sStore = {
     if (rat.canWork()) {
       if (targets.length === 0) { targets = rat.room.find(FIND_STRUCTURES, {
         filter: (structure) => structure.structureType === STRUCTURE_EXTENSION  && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+        target = rat.pos.findClosestByRange(targets);
       }
       if (targets.length === 0) { targets = rat.room.find(FIND_STRUCTURES, {
         filter: (structure) => structure.structureType === STRUCTURE_SPAWN      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+        target = rat.pos.findClosestByRange(targets);
       }
       if (targets.length === 0) { targets = rat.room.find(FIND_STRUCTURES, {
         filter: (structure) => structure.structureType === STRUCTURE_TOWER      && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
+        target = rat.pos.findClosestByRange(targets);
       }
     }
 
     // If the rat is empty then unset all the things.
     if (rat.store.getUsedCapacity() === 0) { rat.clearTask(); }
     // If there are any targets store in order above..
-    else if (targets.length > 0) {
-      const target = rat.pos.findClosestByRange(targets);
+    else if (target) {
       const results = rat.transfer(target, RESOURCE_ENERGY);
       if (results === ERR_NOT_IN_RANGE)   { move.moveTo(rat, target, '#aaaaaa');}
       else if (results === ERR_FULL)      { rat.clearTarget(); }
