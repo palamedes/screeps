dc7fddb3999d4589353bad3cfe200762daca9676
palamedes <jason.ellis@yahoo.com>
2023-03-30 19:37:55 -0400

    Adding a "take all" method for creeps

--- rat.extensions.js
+++ rat.extensions.js
@@ -1,3 +1,4 @@
+const move = require("./skaven.move");
 
 Creep.prototype.getAvailableSpawn = function() {
   const spawns = this.room.find(FIND_MY_STRUCTURES, {
@@ -26,4 +27,46 @@ Creep.prototype.setTask = function(task) { this.memory.task = task; this.memory.
   if (task === 'store')   { this.say('🔋'); }
   if (task === 'renew')   { this.say('⌛'); this.memory.renews--; }
   if (task === 'harvest') { this.say('⚡'); this.setTarget(null); }
+}
+
+Creep.prototype.takeAllFrom = function(target) {
+  let results = [];
+  const store = Object.keys(target.store);
+  for (let i = 0; i < store.length; i++) {
+    if (store[i] === 'energy')  results.push(this.takeFrom(target, RESOURCE_ENERGY));
+    if (store[i] === 'U')       results.push(this.takeFrom(target, RESOURCE_UTRIUM));
+    if (store[i] === 'K')       results.push(this.takeFrom(target, RESOURCE_KEANIUM));
+    if (store[i] === 'L')       results.push(this.takeFrom(target, RESOURCE_LEMERGIUM));
+    if (store[i] === 'Z')       results.push(this.takeFrom(target, RESOURCE_ZYNTHIUM));
+    if (store[i] === 'O')       results.push(this.takeFrom(target, RESOURCE_OXYGEN));
+    if (store[i] === 'H')       results.push(this.takeFrom(target, RESOURCE_HYDROGEN));
+    if (store[i] === 'X')       results.push(this.takeFrom(target, RESOURCE_CATALYST));
+    if (store[i] === 'UH')      results.push(this.takeFrom(target, RESOURCE_UTRIUM_HYDRIDE));
+    if (store[i] === 'UO')      results.push(this.takeFrom(target, RESOURCE_UTRIUM_OXIDE));
+    if (store[i] === 'KH')      results.push(this.takeFrom(target, RESOURCE_KEANIUM_HYDRIDE));
+    if (store[i] === 'KO')      results.push(this.takeFrom(target, RESOURCE_KEANIUM_OXIDE));
+    if (store[i] === 'LH')      results.push(this.takeFrom(target, RESOURCE_LEMERGIUM_HYDRIDE));
+    if (store[i] === 'LO')      results.push(this.takeFrom(target, RESOURCE_LEMERGIUM_OXIDE));
+    if (store[i] === 'ZH')      results.push(this.takeFrom(target, RESOURCE_ZYTHNIUM_HYDRIDE));
+    if (store[i] === 'ZO')      results.push(this.takeFrom(target, RESOURCE_ZYTHNIUM_OXIDE));
+    if (store[i] === 'GH')      results.push(this.takeFrom(target, RESOURCE_GHODIUM_HYDRIDE));
+    if (store[i] === 'GO')      results.push(this.takeFrom(target, RESOURCE_GHODIUM_OXIDE));
+    if (store[i] === 'XUH2O')   results.push(this.takeFrom(target, RESOURCE_UTRIUM_ACID));
+    if (store[i] === 'XUHO2')   results.push(this.takeFrom(target, RESOURCE_UTRIUM_ALKALIDE));
+    if (store[i] === 'XKH2O')   results.push(this.takeFrom(target, RESOURCE_KEANIUM_ACID));
+    if (store[i] === 'XKHO2')   results.push(this.takeFrom(target, RESOURCE_KEANIUM_ALKALIDE));
+    if (store[i] === 'XLH2O')   results.push(this.takeFrom(target, RESOURCE_LEMERGIUM_ACID));
+    if (store[i] === 'XLHO2')   results.push(this.takeFrom(target, RESOURCE_LEMERGIUM_ALKALIDE));
+    if (store[i] === 'XZH2O')   results.push(this.takeFrom(target, RESOURCE_ZYTHNIUM_ACID));
+    if (store[i] === 'XZHO2')   results.push(this.takeFrom(target, RESOURCE_ZYTHNIUM_ALKALIDE));
+    if (store[i] === 'XGH2O')   results.push(this.takeFrom(target, RESOURCE_GHODIUM_ACID));
+    if (store[i] === 'XGHO2')   results.push(this.takeFrom(target, RESOURCE_GHODIUM_ALKALIDE));
+  }
+  return results;
+}
+Creep.prototype.takeFrom = function(target, resource) {
+  let results = null;
+  if (target instanceof Resource && target.energy > 0) { results = rat.pickup(target); }
+  if (target instanceof StructureContainer && target.store[resource] > 0) { results = rat.withdraw(target, resource); }
+  return results;
 }
