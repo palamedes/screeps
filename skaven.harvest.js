28a3a6e412282d88366d4d5ba0982277b3d7a2cb
palamedes <jason.ellis@yahoo.com>
2023-03-30 20:21:25 -0400

    res

--- skaven.harvest.js
+++ skaven.harvest.js
@@ -78,6 +78,7 @@ let sHarvest = {
           } else {
             res = rat.takeAllFrom(target);
           }
+          console.log(res);
           // res == NULL if it's just being dropped to the ground (Harvester) Ignore it.
           if (res.includes(ERR_NOT_IN_RANGE)) {
             console.log('ERROR: Not in range?!  How....');
