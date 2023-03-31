99dbb853920866f42d9f5d37dc712d778e32dc9f
palamedes <jason.ellis@yahoo.com>
2023-03-31 08:16:06 -0400

    test

--- skaven.harvest.js
+++ skaven.harvest.js
@@ -23,6 +23,7 @@ let sHarvest = {
         const containers = rat.room.find(FIND_TOMBSTONES, {
           filter: tombstone => { const totalResources = _.sum(tombstone.store); return totalResources > 0; }
         });
+        console.log('me', tombstone, tomestone.store)
         if (containers.length > 0) {
           rat.memory.myTargetId = rat.pos.findClosestByRange(containers).id;
         }
