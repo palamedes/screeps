3e0563688256e07a473fbf8275c19806d3094ea8
palamedes <jason.ellis@yahoo.com>
2023-03-30 20:33:12 -0400

    name

--- skaven.store.js
+++ skaven.store.js
@@ -22,6 +22,7 @@ let sStore = {
 
     // all other rats, probably a slave, store it somewhere else.
     if (rat.canWork()) {
+      console.log(rat.name);
       if (rat.carryingNonEnergyResource()) { targets = rat.room.find(FIND_STRUCTURES, {
           filter: (structure) => structure.structureType === STRUCTURE_STORAGE    && structure.store.getFreeCapacity(RESOURCE_ENERGY) > 0 });
         target = rat.pos.findClosestByRange(targets);
