357e57ed2874e3061648ad5eb00a2762628ec4f6
palamedes <jason.ellis@yahoo.com>
2023-03-31 22:13:13 -0400

    Commenting things out

--- skaven.js
+++ skaven.js
@@ -2,49 +2,49 @@
 /** Skaven! */
 var skaven = {
   // Skitter!  As all good rats do...
-  skitter: rat => {
-    // If we are a slave, and we have been spawned...
-    if (rat.memory.role === 'slave' && !rat.spawning) {
-      // Get our list of slaves
-      let slaves = _.filter(Game.creeps, (rat) => rat.memory.role === 'slave');
-      // If our ticks to live is down to 50, stop what you're doing and go solve that by renewing at your spawn
-      if (rat.ticksToLive <= 50 && rat.memory.task !== 'renew' && rat.room.controller.level >= 4 && rat.memory.renews > 0) {
-        if (Game.rooms[rat.memory.homeRoom].energyAvailable > 100) { rat.setTask('renew'); }
-      }
-
-      // Rat needs to decide what it should be doing..
-      if (!rat.memory.task) {
-        // Harvester: if this rat can't carry, then he's a harvester.. go do that.
-        if (rat.cannotCarry()) { rat.setTask('harvest'); }
-
-        // Hauler: If rat has less than 40% free capacity ( at least 60% full ) then go store it until empty
-        else if (rat.cannotWork() && (rat.store.getFreeCapacity() / rat.store.getCapacity()) < 0.4) {
-          rat.setTask('storeUntilEmpty');
-        }
-
-        // If rat has less than 80% free capacity ( at least 20% energy ) then go do some work.. Else harvest.
-        else if (rat.canWork() && rat.canCarry() && (rat.store.getFreeCapacity() / rat.store.getCapacity()) < 0.8) {
-          // Upgrade Controller
-          if (rat.shouldWeUpgrade(slaves)) { rat.setTask('upgrade'); }
-          // Construction
-          else if (rat.shouldWeBuild(slaves)) { rat.setTask('build'); }
-          // Repair
-          else if (rat.shouldWeRepair(slaves)) { rat.setTask('repair'); }
-          else {
-            if (rat.shouldWeUpgradeAnyway() && !rat.carryingNonEnergyResource()) {
-              rat.setTask('upgrade');
-            } else {
-              rat.setTask('store');
-            }
-          }
-        } else {
-          rat.setTask('harvest');
-        }
-      }
-      // Okay rat... Do something..
-      rat.skitter();
-    }
-  },
+  // skitter: rat => {
+  //   // If we are a slave, and we have been spawned...
+  //   if (rat.memory.role === 'slave' && !rat.spawning) {
+  //     // Get our list of slaves
+  //     let slaves = _.filter(Game.creeps, (rat) => rat.memory.role === 'slave');
+  //     // If our ticks to live is down to 50, stop what you're doing and go solve that by renewing at your spawn
+  //     if (rat.ticksToLive <= 50 && rat.memory.task !== 'renew' && rat.room.controller.level >= 4 && rat.memory.renews > 0) {
+  //       if (Game.rooms[rat.memory.homeRoom].energyAvailable > 100) { rat.setTask('renew'); }
+  //     }
+  //
+  //     // Rat needs to decide what it should be doing..
+  //     if (!rat.memory.task) {
+  //       // Harvester: if this rat can't carry, then he's a harvester.. go do that.
+  //       if (rat.cannotCarry()) { rat.setTask('harvest'); }
+  //
+  //       // Hauler: If rat has less than 40% free capacity ( at least 60% full ) then go store it until empty
+  //       else if (rat.cannotWork() && (rat.store.getFreeCapacity() / rat.store.getCapacity()) < 0.4) {
+  //         rat.setTask('storeUntilEmpty');
+  //       }
+  //
+  //       // If rat has less than 80% free capacity ( at least 20% energy ) then go do some work.. Else harvest.
+  //       else if (rat.canWork() && rat.canCarry() && (rat.store.getFreeCapacity() / rat.store.getCapacity()) < 0.8) {
+  //         // Upgrade Controller
+  //         if (rat.shouldWeUpgrade(slaves)) { rat.setTask('upgrade'); }
+  //         // Construction
+  //         else if (rat.shouldWeBuild(slaves)) { rat.setTask('build'); }
+  //         // Repair
+  //         else if (rat.shouldWeRepair(slaves)) { rat.setTask('repair'); }
+  //         else {
+  //           if (rat.shouldWeUpgradeAnyway() && !rat.carryingNonEnergyResource()) {
+  //             rat.setTask('upgrade');
+  //           } else {
+  //             rat.setTask('store');
+  //           }
+  //         }
+  //       } else {
+  //         rat.setTask('harvest');
+  //       }
+  //     }
+  //     // Okay rat... Do something..
+  //     rat.skitter();
+  //   }
+  // },
 
   // Should we store power?
   shouldWeStore: (rat, slaves) => {
