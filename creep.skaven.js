bb18111be70e6379cb87bd31637ef0a171b4a782
palamedes <jason.ellis@yahoo.com>
2023-03-31 22:06:45 -0400

    Upgrading

--- creep.skaven.js
+++ creep.skaven.js
@@ -15,3 +15,29 @@ Creep.prototype.shouldWeBuild = function(slaves) {
   }
   return false;
 }
+
+// Should we upgrade the controller?
+// Are we bored? Do we have enough slaves? Do we not have enough active? Are we full everywhere?
+Creep.prototype.shouldWeUpgrade = function(slaves) {
+  const upgradeTarget = this.room.controller;
+  if (upgradeTarget && this.canCarry() && this.canWork()) {
+    // if the rat has been sleeping on the job, go make him upgrade..
+    if (this.memory.slept > 2) return true;
+    // Do we have 80% of max slaves?
+    const enoughSlaves = slaves.length >= (Memory.rooms[this.room.name].maxSlaves*0.8);
+    // Are less than 25% doing the work?
+    const notEnoughActive = Creep.numActive('upgrade') < (Memory.rooms[this.room.name].maxSlaves * 0.25);
+    // Is No one upgrading?!
+    const noSlavesUpgrading = Creep.numActive('upgrade') === 0;
+    // Are we full energy?
+    const fullEnergy = this.room.energyAvailable === Memory.rooms[this.room.name].maxEnergy
+    // Decide
+    if (enoughSlaves && notEnoughActive && fullEnergy && noSlavesUpgrading) return true;
+  }
+  return false;
+}
+// While I'm not the designated Upgrader there is no construction, nothing to repair, and the extensions, spawns and towers are full..
+Creep.prototype.shouldWeUpgradeAnyway = function() {
+  const fullEnergy = this.room.energyAvailable === Memory.rooms[this.room.name].maxEnergy;
+  return fullEnergy && this.canWork();
+}
--- skaven.js
+++ skaven.js
@@ -25,13 +25,13 @@ var skaven = {
         // If rat has less than 80% free capacity ( at least 20% energy ) then go do some work.. Else harvest.
         else if (rat.canWork() && rat.canCarry() && (rat.store.getFreeCapacity() / rat.store.getCapacity()) < 0.8) {
           // Upgrade Controller
-          if (skaven.shouldWeUpgrade(rat, slaves)) { rat.setTask('upgrade'); }
+          if (rat.shouldWeUpgrade(slaves)) { rat.setTask('upgrade'); }
           // Construction
           else if (rat.shouldWeBuild(slaves)) { rat.setTask('build'); }
           // Repair
           else if (skaven.shouldWeRepair(rat, slaves)) { rat.setTask('repair'); }
           else {
-            if (skaven.shouldWeUpgradeAnyway(rat) && !rat.carryingNonEnergyResource()) {
+            if (rat.shouldWeUpgradeAnyway() && !rat.carryingNonEnergyResource()) {
               rat.setTask('upgrade');
             } else {
               rat.setTask('store');
@@ -68,31 +68,31 @@ var skaven = {
   //   return false;
   // },
 
-  // Should we upgrade the controller?
-  // Are we bored? Do we have enough slaves? Do we not have enough active? Are we full everywhere?
-  shouldWeUpgrade: (rat, slaves) => {
-    const upgradeTarget = rat.room.controller;
-    if (upgradeTarget && rat.canCarry() && rat.canWork()) {
-      // if the rat has been sleeping on the job, go make him upgrade..
-      if (rat.memory.slept > 2) return true;
-      // Do we have 80% of max slaves?
-      const enoughSlaves = slaves.length >= (Memory.rooms[rat.room.name].maxSlaves*0.8);
-      // Are less than 25% doing the work?
-      const notEnoughActive = Creep.numActive('upgrade') < (Memory.rooms[rat.room.name].maxSlaves * 0.25);
-      // Is No one upgrading?!
-      const noSlavesUpgrading = Creep.numActive('upgrade') === 0;
-      // Are we full energy?
-      const fullEnergy = rat.room.energyAvailable === Memory.rooms[rat.room.name].maxEnergy
-      // Decide
-      if (enoughSlaves && notEnoughActive && fullEnergy && noSlavesUpgrading) return true;
-    }
-    return false;
-  },
+  // // Should we upgrade the controller?
+  // // Are we bored? Do we have enough slaves? Do we not have enough active? Are we full everywhere?
+  // shouldWeUpgrade: (rat, slaves) => {
+  //   const upgradeTarget = rat.room.controller;
+  //   if (upgradeTarget && rat.canCarry() && rat.canWork()) {
+  //     // if the rat has been sleeping on the job, go make him upgrade..
+  //     if (rat.memory.slept > 2) return true;
+  //     // Do we have 80% of max slaves?
+  //     const enoughSlaves = slaves.length >= (Memory.rooms[rat.room.name].maxSlaves*0.8);
+  //     // Are less than 25% doing the work?
+  //     const notEnoughActive = Creep.numActive('upgrade') < (Memory.rooms[rat.room.name].maxSlaves * 0.25);
+  //     // Is No one upgrading?!
+  //     const noSlavesUpgrading = Creep.numActive('upgrade') === 0;
+  //     // Are we full energy?
+  //     const fullEnergy = rat.room.energyAvailable === Memory.rooms[rat.room.name].maxEnergy
+  //     // Decide
+  //     if (enoughSlaves && notEnoughActive && fullEnergy && noSlavesUpgrading) return true;
+  //   }
+  //   return false;
+  // },
   // While I'm not the designated Upgrader there is no construction, nothing to repair, and the extensions, spawns and towers are full..
-  shouldWeUpgradeAnyway: rat => {
-    const fullEnergy = rat.room.energyAvailable === Memory.rooms[rat.room.name].maxEnergy;
-    return fullEnergy && rat.canWork()
-  },
+  // shouldWeUpgradeAnyway: rat => {
+  //   const fullEnergy = rat.room.energyAvailable === Memory.rooms[rat.room.name].maxEnergy;
+  //   return fullEnergy && rat.canWork()
+  // },
 
   // Should we repair something?
   // If we have 50% or more rats, and we have 20% or less repairing and there are no towers...
