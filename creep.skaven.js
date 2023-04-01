c5d26f00bb46d744f9442634be3cfeffa2cb81b3
palamedes <jason.ellis@yahoo.com>
2023-03-31 22:13:22 -0400

    Moving skitter to creep

--- creep.skaven.js
+++ creep.skaven.js
@@ -1,4 +1,50 @@
 
+// Run the Skaven (Slaves of all types)
+Creep.prototype.skitter = function() {
+  // If we are a slave, and we have been spawned...
+  if (this.memory.role === 'slave' && !this.spawning) {
+    // Get our list of slaves
+    let slaves = _.filter(Game.creeps, (rat) => this.memory.role === 'slave');
+    // If our ticks to live is down to 50, stop what you're doing and go solve that by renewing at your spawn
+    if (this.ticksToLive <= 50 && this.memory.task !== 'renew' && this.room.controller.level >= 4 && this.memory.renews > 0) {
+      if (Game.rooms[this.memory.homeRoom].energyAvailable > 100) { this.setTask('renew'); }
+    }
+
+    // Rat needs to decide what it should be doing..
+    if (!this.memory.task) {
+      // Harvester: if this rat can't carry, then he's a harvester.. go do that.
+      if (this.cannotCarry()) { this.setTask('harvest'); }
+
+      // Hauler: If rat has less than 40% free capacity ( at least 60% full ) then go store it until empty
+      else if (this.cannotWork() && (this.store.getFreeCapacity() / this.store.getCapacity()) < 0.4) {
+        this.setTask('storeUntilEmpty');
+      }
+
+      // If rat has less than 80% free capacity ( at least 20% energy ) then go do some work.. Else harvest.
+      else if (this.canWork() && this.canCarry() && (this.store.getFreeCapacity() / this.store.getCapacity()) < 0.8) {
+        // Upgrade Controller
+        if (this.shouldWeUpgrade(slaves)) { this.setTask('upgrade'); }
+        // Construction
+        else if (this.shouldWeBuild(slaves)) { this.setTask('build'); }
+        // Repair
+        else if (this.shouldWeRepair(slaves)) { this.setTask('repair'); }
+        // Store (or Upgrade anyway if bored)
+        else {
+          if (this.shouldWeUpgradeAnyway() && !this.carryingNonEnergyResource()) {
+            this.setTask('upgrade');
+          } else {
+            this.setTask('store');
+          }
+        }
+      } else {
+        this.setTask('harvest');
+      }
+    }
+    // Okay this... Do something..
+    this.skitter();
+  }
+}
+  
 // Should we build something?
 // If we have 50% or more rats, and we don't have more than 50% doing the work
 Creep.prototype.shouldWeBuild = function(slaves) {
