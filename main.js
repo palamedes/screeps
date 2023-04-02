b8fa877e8d0f5c418a0ddbb1200056217f2123fd
palamedes <jason.ellis@yahoo.com>
2023-04-02 19:36:13 -0400

    Refactor of the main loop to move some stuff out.

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ require('creep.task.store');
 require('creep.task.harvest');
 require('creep.skaven');
 require('creep.skaven.slave');
+require('creep.skaven.runner');
 
 require('room');
 require('room.summon.slave');
@@ -24,57 +25,29 @@ module.exports.loop = function () {
 
   // Define some universal values
   const slaves = _.filter(Game.creeps, (rat) => rat.memory.role === 'slave') ;
-  const ogres  = _.filter(Game.creeps, (rat) => rat.memory.role === 'ogre');
+  const runners = _.filter(Game.creeps, (rat) => rat.memory.role === 'runner') ;
 
-  let statusUpdate = "";
   // Iterate through each room we are in
   for (let i in Memory.roomsList) {
-
-    // let spawn = Game.spawns[Object.keys(Game.spawns)[i]]
-    const roomName = Memory.roomsList[i];
-    const room = Game.rooms[roomName];
-    // Setup the room if it hasn't been yet
-    const mem = room.setMemory(room);
-    // Get energy amounts
+    const roomName = Memory.roomsList[i], room = Game.rooms[roomName], mem = room.setMemory(room);
     const spawns = room.find(FIND_MY_SPAWNS);
     const extensions = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION } });
+    const containers = room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_CONTAINER } });
     const totalSpawnsCapacity = _.sum(spawns, (s) => s.energyCapacity);
     const totalExtensionsCapacity = _.sum(extensions, (e) => e.energyCapacity);
     Memory.rooms[roomName].maxEnergy = totalSpawnsCapacity + totalExtensionsCapacity;
-    const containers = room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_CONTAINER } });
     Memory.rooms[roomName].containerAvailability = _.sum(containers, (c) => c.store.getFreeCapacity(RESOURCE_ENERGY));
-
-    statusUpdate = 'Room "'+room.name+'" has ' + room.energyAvailable + '/' + Memory.rooms[roomName].maxEnergy + ' energy';
-    statusUpdate += (slaves.length > 0) ? ' ~ Slaves: ' + slaves.length + '/' + Memory.rooms[room.name].maxSlaves : '';
-    statusUpdate += (ogres.length > 0) ? ', Ogres: ' + ogres.length : '';
-
-    // Get the data we need to determine if we need more slaves
-
-
-    // Rejigger max slaves for this room based on the number of suckle points RCL 1 to 4
-    let numSucklePoints = () => { return Object.values(Memory.rooms[room.name].sources).reduce((acc, val) => acc + val.length, 0); }
-    Memory.rooms[room.name].numSucklePoints = numSucklePoints();
+    Memory.rooms[roomName].numSucklePoints = Object.values(Memory.rooms[room.name].sources).reduce((acc, val) => acc + val.length, 0);
     Memory.rooms[room.name].maxSlaves = (Memory.rooms[room.name].numSucklePoints * 2) + Memory.rooms[room.name].numSucklePoints;
-
     // At RCL 5+ we need to start being smarter about our rats and who does what work.
     if (room.controller.level >= 5) Memory.rooms[room.name].maxSlaves = 5;
-
-    // Spawn a skaven slave
-    if ((slaves.length < 2 || (slaves.length < mem.maxSlaves && room.energyAvailable >= Memory.rooms[room.name].maxEnergy)) && room.energyAvailable >= 200) {
-      if (room.summonSlave({ homeRoom: room.name, version: room.controller.level })) {
-        statusUpdate += ' ~ Spawning new Slave ('+room.energyAvailable+')'
-      }
-    }
-
-    // Based on the status of the room
+    // Spawn something if we need to
+    Creep.skaven.slave.summon(room, slaves);      // Spawn a Skaven Slave
+    Creep.skaven.runner.summon(room, runners);    // Spawn a Skaven Gutter Runner
+    // Run the room..
     room.run();
   }
 
-  // Work the rats!
-  for(let name in Game.creeps) {
-    Game.creeps[name].run(slaves);
-  }
-
-  // Report what's up..
-  // console.log(statusUpdate);
+  // Run the rats!
+  for(let name in Game.creeps) { Game.creeps[name].run(slaves, runners); }
 }
