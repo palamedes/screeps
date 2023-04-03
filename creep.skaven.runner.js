992ac64fe18ce4dc0226cf9ab0519b270ce8929f
palamedes <jason.ellis@yahoo.com>
2023-04-02 20:22:26 -0400

    Fix

--- creep.skaven.runner.js
+++ creep.skaven.runner.js
@@ -46,9 +46,9 @@ Creep.prototype.skaven.runner.findRoom = function() {
 
 // Summon a Skaven Gutter Runner if we need to...
 Creep.summonSkavenRunner = function(room, runners) {
-  console.log('should we summon a runner?', room.controllerLevel, runners.length, room.energyAvailable);
+  console.log('should we summon a runner?', room.controller.level, runners.length, room.energyAvailable);
   // Summon a Gutter Runner if we meet certain criteria
-  if (room.controllerLevel >= 5 && runners.length === 0 && room.energyAvailable > 1000) {
+  if (room.controller.level >= 5 && runners.length === 0 && room.energyAvailable > 1000) {
     console.log('yes...')
     const ratName = 'Runner-' + Game.time + '-' + this.energyAvailable;
     const ratBrain = { memory: { role: 'runner', renews: 0, spawn: { id: ratSpawn.id, name: ratSpawn.name }, task: null, slept: 0, taskAttempt: 0, moveAttempt: 0 } };
