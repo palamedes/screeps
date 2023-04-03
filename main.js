ed6e3def8582f6d29e91e9ea46d8e885cf42c200
palamedes <jason.ellis@yahoo.com>
2023-04-02 22:01:15 -0400

    IF we no longer contorl a room.. remove it from memory..

--- main.js
+++ main.js
@@ -22,6 +22,10 @@ module.exports.loop = function () {
   for(var name in Memory.creeps) { if(!Game.creeps[name]) { delete Memory.creeps[name]; }}
   // Get all our rooms (this should just be 1 room at the start of the game.. the rest will be added later)
   Memory.roomsList = Memory.roomsList || _.uniq(_.map(Game.spawns, (spawn) => spawn.room.name));
+  // Check to make sure we still own those rooms
+  if (Game.rooms[roomName] && room.controller && room.controller.my) {
+    console.log("You own room", roomName);
+  }
 
   // Define some universal values
   const slaves = _.filter(Game.creeps, (rat) => rat.memory.role === 'slave') ;
@@ -29,23 +33,31 @@ module.exports.loop = function () {
 
   // Iterate through each room we are in
   for (let i in Memory.roomsList) {
-    const roomName = Memory.roomsList[i], room = Game.rooms[roomName], mem = room.setMemory(room);
-    const spawns = room.find(FIND_MY_SPAWNS);
-    const extensions = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION } });
-    const containers = room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_CONTAINER } });
-    const totalSpawnsCapacity = _.sum(spawns, (s) => s.energyCapacity);
-    const totalExtensionsCapacity = _.sum(extensions, (e) => e.energyCapacity);
-    Memory.rooms[roomName].maxEnergy = totalSpawnsCapacity + totalExtensionsCapacity;
-    Memory.rooms[roomName].containerAvailability = _.sum(containers, (c) => c.store.getFreeCapacity(RESOURCE_ENERGY));
-    Memory.rooms[roomName].numSucklePoints = Object.values(Memory.rooms[room.name].sources).reduce((acc, val) => acc + val.length, 0);
-    Memory.rooms[room.name].maxSlaves = (Memory.rooms[room.name].numSucklePoints * 2) + Memory.rooms[room.name].numSucklePoints;
-    // At RCL 5+ we need to start being smarter about our rats and who does what work.
-    if (room.controller.level >= 5) Memory.rooms[room.name].maxSlaves = 5;
-    // Spawn something if we need to
-    Creep.summonSkavenSlave(room, slaves);      // Spawn a Skaven Slave
-    Creep.summonSkavenRunner(room, runners);    // Spawn a Skaven Gutter Runner
-    // Run the room..
-    room.run();
+    const roomName = Memory.roomsList[i], room = Game.rooms[roomName];
+    if (room && room.controller && room.controller.my) {
+      const mem = room.setMemory(room);
+      const spawns = room.find(FIND_MY_SPAWNS);
+      const extensions = room.find(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_EXTENSION } });
+      const containers = room.find(FIND_STRUCTURES, { filter: { structureType: STRUCTURE_CONTAINER } });
+      const totalSpawnsCapacity = _.sum(spawns, (s) => s.energyCapacity);
+      const totalExtensionsCapacity = _.sum(extensions, (e) => e.energyCapacity);
+      Memory.rooms[roomName].maxEnergy = totalSpawnsCapacity + totalExtensionsCapacity;
+      Memory.rooms[roomName].containerAvailability = _.sum(containers, (c) => c.store.getFreeCapacity(RESOURCE_ENERGY));
+      Memory.rooms[roomName].numSucklePoints = Object.values(Memory.rooms[room.name].sources).reduce((acc, val) => acc + val.length, 0);
+      Memory.rooms[room.name].maxSlaves = (Memory.rooms[room.name].numSucklePoints * 2) + Memory.rooms[room.name].numSucklePoints;
+      // At RCL 5+ we need to start being smarter about our rats and who does what work.
+      if (room.controller.level >= 5) Memory.rooms[room.name].maxSlaves = 5;
+      // Spawn something if we need to
+      Creep.summonSkavenSlave(room, slaves);      // Spawn a Skaven Slave
+      Creep.summonSkavenRunner(room, runners);    // Spawn a Skaven Gutter Runner
+      // Run the room..
+      room.run();
+    } else {
+      if (Memory.roomsList.indexOf(roomName) !== -1) {
+        Memory.roomsList.splice(Memory.roomsList.indexOf(roomName), 1);
+        delete Memory.rooms[roomName];
+      }
+    }
   }
 
   // Run the rats!
