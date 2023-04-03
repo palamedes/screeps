07f37f8068ce4131f4e5c313cae79cbcccc5fd7c
palamedes <jason.ellis@yahoo.com>
2023-04-02 20:50:43 -0400

    Dont have room

--- creep.skaven.js
+++ creep.skaven.js
@@ -19,7 +19,7 @@ Creep.prototype.run = function(slaves, runners) {
   // If we are a Skaven Slave, and we have been spawned...
   if (this.memory.role === 'slave' && !this.spawning) { this.skaven.slave.skitter.bind(this)(slaves); }
   // If we are a Skaven Gutter Runner, and we have been spawned...
-  if (this.memory.role === 'runner' && !this.spawning) { this.skaven.runner.skitter.bind(this)(room, runners); }
+  if (this.memory.role === 'runner' && !this.spawning) { this.skaven.runner.skitter.bind(this)(runners); }
 }
 // Define name space for various skaven types
 Creep.prototype.skaven = {slave: {}, runner: {}};
