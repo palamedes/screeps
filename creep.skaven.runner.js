d4df3f898a028fd1dba2399bafdce5fc4ed8538a
palamedes <jason.ellis@yahoo.com>
2023-04-02 21:33:48 -0400

    test

--- creep.skaven.runner.js
+++ creep.skaven.runner.js
@@ -56,7 +56,7 @@ Creep.prototype.skaven.runner.claimRoom = function() {
     }
   }
 
-  console.log('here', this.room.controller.my);
+  console.log('here', this.room.controller.my, !Memory.roomsList.includes(this.room.name), this.room.controller);
   if (this.room.controller && !this.room.controller.my && !Memory.roomsList.includes(this.room.name)) {
     Memory.roomsList.push(this.room.name);
   }
