191b5ad27f89b7369ee9fd02f7095a6dbe3920c6
palamedes <jason.ellis@yahoo.com>
2023-04-03 21:12:59 -0400

    Updating the main loop

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ require('creep.task.harvest');
 require('creep.skaven');
 require('creep.skaven.slave');
 require('creep.skaven.runner');
+require('creep.skaven.engineer');
 
 require('room');
 require('room.summon.slave');
@@ -22,9 +23,10 @@ module.exports.loop = function () {
   for(var name in Memory.creeps) { if(!Game.creeps[name]) { delete Memory.creeps[name]; }}
   // Get all our rooms (this should just be 1 room at the start of the game.. the rest will be added later)
   Memory.roomsList = Memory.roomsList || _.uniq(_.map(Game.spawns, (spawn) => spawn.room.name));
-  // Define some universal values
+  // Get our various skaven roles
   const slaves = _.filter(Game.creeps, (rat) => rat.memory.role === 'slave') ;
   const runners = _.filter(Game.creeps, (rat) => rat.memory.role === 'runner') ;
+  const engineers = _.filter(Game.creeps, (rat) => rat.memory.role === 'engineers') ;
 
   // Iterate through each room we are in
   for (let i in Memory.roomsList) {
@@ -42,9 +44,10 @@ module.exports.loop = function () {
       Memory.rooms[room.name].maxSlaves = (Memory.rooms[room.name].numSucklePoints * 2) + Memory.rooms[room.name].numSucklePoints;
       // At RCL 5+ we need to start being smarter about our rats and who does what work.
       if (room.controller.level >= 5) Memory.rooms[room.name].maxSlaves = 5;
-      // Spawn something if we need to
-      Creep.summonSkavenSlave(room, slaves);      // Spawn a Skaven Slave
-      Creep.summonSkavenRunner(room, runners);    // Spawn a Skaven Gutter Runner
+      // Spawn something if we need to ... Each role knows how to summon itself
+      Creep.summonSkavenSlave(room, slaves);        // Spawn a Skaven Slave
+      Creep.summonSkavenRunner(room, runners);      // Spawn a Skaven Gutter Runner
+      Creep.summonSkavenEngineer(room, engineers);  // Spawn a Skaven Engineer
       // Run the room..
       room.run();
     } else {
