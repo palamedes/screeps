dc0e85548c00dd13133890ab308f0081ac686bb0
palamedes <jason.ellis@yahoo.com>
2026-02-12 18:41:54 -0500

    Fix for miner stuck

--- creep.miner.js
+++ creep.miner.js
@@ -1,10 +1,24 @@
 Creep.prototype.runMiner = function () {
+
   if (!this.memory.sourceId) {
     const sources = this.room.find(FIND_SOURCES);
-    this.memory.sourceId = sources[0].id;
+
+    for (const source of sources) {
+      const minersOnSource = Object.values(Game.creeps).filter(c =>
+        c.memory.role === 'miner' &&
+        c.memory.sourceId === source.id
+      );
+
+      if (minersOnSource.length === 0) {
+        this.memory.sourceId = source.id;
+        break;
+      }
+    }
   }
 
   const source = Game.getObjectById(this.memory.sourceId);
+
+  if (!source) return;
 
   if (this.harvest(source) === ERR_NOT_IN_RANGE) {
     this.moveTo(source);
