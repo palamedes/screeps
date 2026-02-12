f2c8df3530c63aed81c003e26d1c26377a386ed2
palamedes <jason.ellis@yahoo.com>
2026-02-12 18:19:53 -0500

    Dont harvest any more.

--- creep.worker.js
+++ creep.worker.js
@@ -1,4 +1,9 @@
 Creep.prototype.runWorker = function () {
+
+  if (this.memory.job && this.memory.job.type === 'HARVEST') {
+    delete this.memory.job;
+  }
+
   if (!this.memory.job) {
     this.memory.job = this.findJob();
   }
