03b64b7b7db0612faf240e275d04754b716e9769
palamedes <jason.ellis@yahoo.com>
2026-02-12 16:41:26 -0500

    Fixng

--- creep.js
+++ creep.js
@@ -27,9 +27,7 @@ Creep.prototype.runJob = function () {
 
   switch (job.type) {
     case 'HARVEST':
-      if (this.harvest(target) === ERR_NOT_IN_RANGE) {
-        this.moveTo(target);
-      }
+      this.runWorkerCycle(target);
       break;
 
     case 'UPGRADE':
@@ -49,5 +47,40 @@ Creep.prototype.runJob = function () {
   if (job.type === 'BUILD' && target.progress === target.progressTotal) {
     this.memory.job = null;
   }
-
 };
+
+Creep.prototype.runWorkerCycle = function(source) {
+
+  // State switching
+  if (this.memory.working && this.store[RESOURCE_ENERGY] === 0) {
+    this.memory.working = false;
+  }
+
+  if (!this.memory.working && this.store.getFreeCapacity() === 0) {
+    this.memory.working = true;
+  }
+
+  // Harvest mode
+  if (!this.memory.working) {
+    if (this.harvest(source) === ERR_NOT_IN_RANGE) {
+      this.moveTo(source);
+    }
+    return;
+  }
+
+  // Working mode
+  const spawn = this.room.find(FIND_MY_SPAWNS)[0];
+
+  if (spawn.store.getFreeCapacity(RESOURCE_ENERGY) > 0) {
+    if (this.transfer(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+      this.moveTo(spawn);
+    }
+    return;
+  }
+
+  if (this.upgradeController(this.room.controller) === ERR_NOT_IN_RANGE) {
+    this.moveTo(this.room.controller);
+  }
+};
+
+
