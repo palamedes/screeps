d08d354db0fc36cb1f19f6d8099fd3ee553a042b
palamedes <jason.ellis@yahoo.com>
2026-02-12 18:17:09 -0500

    Slave update

--- creep.slave.js
+++ creep.slave.js
@@ -1,4 +1,13 @@
 Creep.prototype.runSlave = function () {
+
+  // If we are RCL2+, convert to worker
+  if (this.room.controller.level >= 2) {
+    this.memory.role = 'worker';
+    delete this.memory.job;
+    return;
+  }
+
+  // RCL1 behavior
   if (!this.memory.job) {
     this.memory.job = this.findJob();
   }
