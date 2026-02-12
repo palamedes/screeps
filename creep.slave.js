eb148f6861da2fde21c4f327ba0a98fa38047b85
palamedes <jason.ellis@yahoo.com>
2026-02-12 18:19:10 -0500

    Clear jobs

--- creep.slave.js
+++ creep.slave.js
@@ -1,13 +1,16 @@
 Creep.prototype.runSlave = function () {
 
-  // If we are RCL2+, convert to worker
   if (this.room.controller.level >= 2) {
     this.memory.role = 'worker';
+
+    // FULL reset of slave-specific state
     delete this.memory.job;
+    delete this.memory.working;
+    delete this.memory.sourceId;
+
     return;
   }
 
-  // RCL1 behavior
   if (!this.memory.job) {
     this.memory.job = this.findJob();
   }
