1a12b5236cd6017d70ea9f78ca7cdf90b0221903
palamedes <jason.ellis@yahoo.com>
2026-02-12 18:57:59 -0500

    fix

--- room.director.spawn.js
+++ room.director.spawn.js
@@ -60,7 +60,7 @@ module.exports = {
       return;
     }
 
-    if (workers.length < miners.length)
+    if (workers.length < miners.length) {
       spawn.spawnCreep(this.createWorkerBody(room), `worker_${Game.time}`, {
         memory: { role: 'worker' }
       });
