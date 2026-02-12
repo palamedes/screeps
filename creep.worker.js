19fb918d78fbdc2b485c8c820803be83bfe4f595
palamedes <jason.ellis@yahoo.com>
2026-02-12 18:54:20 -0500

    Worker msut work

--- creep.worker.js
+++ creep.worker.js
@@ -1,9 +1,18 @@
 Creep.prototype.runWorker = function () {
 
-  if (this.memory.job && this.memory.job.type === 'HARVEST') {
-    delete this.memory.job;
+  // If empty, go get energy from spawn
+  if (this.store[RESOURCE_ENERGY] === 0) {
+    const spawn = this.room.find(FIND_MY_SPAWNS)[0];
+
+    if (spawn.store[RESOURCE_ENERGY] > 0) {
+      if (this.withdraw(spawn, RESOURCE_ENERGY) === ERR_NOT_IN_RANGE) {
+        this.moveTo(spawn);
+      }
+    }
+    return;
   }
 
+  // Has energy → do job
   if (!this.memory.job) {
     this.memory.job = this.findJob();
   }
