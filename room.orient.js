5bb58cfd77531e1f941a4b47f51ee28afe1857d3
palamedes <jason.ellis@yahoo.com>
2026-02-12 21:16:18 -0500

    refactor orient.

--- room.orient.js
+++ room.orient.js
@@ -3,40 +3,30 @@ const { ROOM_STATE } = require('room.memory');
 Room.prototype.orient = function () {
   const snap = this._snapshot;
 
-  const extensions = snap.structures.filter(
-    s => s.structureType === STRUCTURE_EXTENSION
-  );
-
-  const extensionSites = snap.constructionSites.filter(
-    s => s.structureType === STRUCTURE_EXTENSION
-  );
-
-  const totalExtensions = extensions.length + extensionSites.length;
-
-  const maxExtensions =
-    CONTROLLER_STRUCTURES[STRUCTURE_EXTENSION][snap.rcl] || 0;
-
+  // Threat override
   if (snap.hostiles.length > 0) {
     return this.setState(ROOM_STATE.WAR);
   }
 
+  // Early survival phase
   if (snap.rcl === 1) {
     return this.setState(ROOM_STATE.BOOTSTRAP);
   }
 
-  if (snap.rcl === 2) {
-    if (totalExtensions < 2) {
-      return this.setState(ROOM_STATE.GROW);
-    }
-  } else {
-    if (totalExtensions < maxExtensions) {
-      return this.setState(ROOM_STATE.GROW);
-    }
+  // Economic pressure signal:
+  // If we are frequently energy capped, we should be expanding
+  const energyCapped =
+    snap.energyAvailable === snap.energyCapacityAvailable;
+
+  if (energyCapped) {
+    return this.setState(ROOM_STATE.GROW);
   }
 
+  // If construction is already in progress, remain in growth posture
   if (snap.constructionSites.length > 0) {
     return this.setState(ROOM_STATE.GROW);
   }
 
+  // Otherwise we are in maintenance mode
   return this.setState(ROOM_STATE.STABLE);
 };
