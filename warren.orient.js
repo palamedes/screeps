6352ab368e9e6489da9c3a683eb33b60210c7038
Jason Ellis <jason.ellis@yahoo.com>
2026-02-22 12:03:40 -0500

    Update warren.orient.js

--- warren.orient.js
+++ warren.orient.js
@@ -1,20 +1,32 @@
 const { ROOM_STATE } = require('warren.memory');
 
+// How long to stay in FORTIFY after an attack clears (ticks)
+const FORTIFY_DURATION = 3000;
+
 Room.prototype.orient = function () {
   const snap = this._snapshot;
 
-  // Threat override
+  // --- Threat override ---
   if (snap.hostiles.length > 0) {
+    this._logAttackEvent(snap.hostiles);
     return this.setState(ROOM_STATE.WAR);
   }
 
-  // Early survival phase
+  // --- FORTIFY: recent attack, no active safe mode ---
+  // Hold a defensive posture until ramparts are healthy and some time has passed.
+  const recentAttack = this.memory.lastAttackTick &&
+    (Game.time - this.memory.lastAttackTick) < FORTIFY_DURATION;
+
+  if (recentAttack) {
+    return this.setState(ROOM_STATE.FORTIFY);
+  }
+
+  // --- Early survival phase ---
   if (snap.rcl === 1) {
     return this.setState(ROOM_STATE.BOOTSTRAP);
   }
 
-  // Economic pressure signal:
-  // If we are frequently energy capped, we should be expanding
+  // --- Economic pressure signal ---
   const energyCapped =
     snap.energyAvailable === snap.energyCapacityAvailable;
 
@@ -22,11 +34,9 @@ Room.prototype.orient = function () {
     return this.setState(ROOM_STATE.GROW);
   }
 
-  // If construction is already in progress, remain in growth posture
   if (snap.constructionSites.length > 0) {
     return this.setState(ROOM_STATE.GROW);
   }
 
-  // Otherwise we are in maintenance mode
   return this.setState(ROOM_STATE.STABLE);
 };
